b74366c8fabc6734353ee41f924ec9c0660dd807
Victoria Brovina <117751176+vioviooo@users.noreply.github.com>
2024-12-22 18:56:06 +0300

    some minor changes

--- src/main/java/com/example/codeforces/repository/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.codeforces.repository;
-
-import com.example.codeforces.db.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
